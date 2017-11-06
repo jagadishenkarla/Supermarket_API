08d46425e57c650a0dbaab61799984f1d8ee6015
DESKTOP-3H9CV8V\jagad <jagadish.pasham@gmail.com>
2017-11-05 21:43:42 -0600

    Add All Files

---
+++ app/routes/note_routes.js
@@ -0,0 +1,61 @@
+
+var ObjectID = require('mongodb').ObjectID;
+ 
+
+module.exports = function(app, db) {
+ 
+/*app.post('/onlinefood', (req, res) => {
+    console.log(req.body)
+    res.send('Hello welcome to online super market')
+  });
+*/
+  app.get('/onlinefood/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('supermarket').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      } 
+    });
+  });
+
+  app.put('/onlinefood/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('supermarket').update(details, note, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(note);
+      } 
+    });
+  });
+
+  app.delete('/onlinefood/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('supermarket').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Note ' + id + ' deleted!');
+      } 
+    });
+  });
+
+
+
+  app.post('/onlinefood', (req, res) => {
+    const note = { text: req.body.body, title: req.body.title };
+    db.collection('supermarket').insert(note, (err, result) => {
+      if (err) { 
+        res.send({ 'error': 'An error has occurred' }); 
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
+  });
+};
